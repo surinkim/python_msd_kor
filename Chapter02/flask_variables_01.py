353f7fd08b5d94d2d6ecd8e189cd2a82a0f2b737
Hyunuk <nnhope@hotmail.com>
2018-01-12 01:18:34 +0900

    Update Chapter02

---
+++ Chapter02/flask_variables_01.py
@@ -0,0 +1,14 @@
+#Chapter02-flask_variables_01.py
+from flask import Flask, jsonify 
+ 
+app = Flask(__name__) 
+ 
+@app.route('/api/person/<person_id>')
+def person(person_id):
+    response = jsonify({'Hello': person_id})
+    return response
+
+if __name__ == '__main__': 
+    app.run()
+
+
