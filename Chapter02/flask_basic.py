c7685c7ba7b75fd0aea38e40ff4fbddc665172a6
Hyunuk <nnhope@hotmail.com>
2018-01-11 22:52:19 +0900

    Update Chapter02

---
+++ Chapter02/flask_basic.py
@@ -0,0 +1,11 @@
+#Chapter02-flask_basic.py
+from flask import Flask, jsonify 
+ 
+app = Flask(__name__) 
+ 
+@app.route('/api')
+def my_microservice(): 
+    return jsonify({'Hello': 'World!'}) 
+
+if __name__ == '__main__': 
+    app.run()
---
+++ Chapter02/flask_print.py
@@ -0,0 +1,17 @@
+#Chapter02-flask_print.py
+from flask import Flask, jsonify, request 
+ 
+app = Flask(__name__) 
+ 
+@app.route('/api') 
+def my_microservice(): 
+    print(request) 
+    print(request.environ) 
+    response = jsonify({'Hello': 'World!'}) 
+    print(response) 
+    print(response.data) 
+    return response 
+ 
+if __name__ == '__main__': 
+    print(app.url_map) 
+    app.run() 
