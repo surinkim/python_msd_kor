11140fea88f8a3fd1bb0d1cad9f60e081efb0a1a
Hyunuk <nnhope@hotmail.com>
2018-01-12 13:02:47 +0900

    Update Chapter02

--- Chapter02/flask_variables_02.py
+++ Chapter02/flask_variables_02.py
@@ -3,14 +3,14 @@ from flask import Flask, jsonify, request
 from werkzeug.routing import BaseConverter, ValidationError
 
 _USERS = {'1': 'Tarek', '2': 'Freya'}
-_IDS = {(val, id) for id, val in _USERS.items()}
+_IDS = {val: id for id, val in _USERS.items()}
 
 class RegisteredUser(BaseConverter):
     def to_python(self, value):
         if value in _USERS:
             return _USERS[value]
         raise ValidationError()
-		
+
     def to_url(self, value):
         return _IDS[value]
 
--- Chapter04/test.py
+++
@@ -1,4 +0,0 @@
-from greenlet import greenlet
-def test1(x, y):
-    z = gr2.switch(x+y)
-    print(z)
