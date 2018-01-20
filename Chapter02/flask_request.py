f863d9f7bfd822bfdad4b18e84c48797d1cf7398
Hyunuk <nnhope@hotmail.com>
2018-01-20 20:21:16 +0900

    Update Chatper02

---
+++ Chapter02/flask_request.py
@@ -0,0 +1,16 @@
+# Chapter02
+# flask_request.py
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def auth():
+    print("The raw Authorization header")
+    print(request.environ["HTTP_AUTHORIZATION"])
+    print("Flask's Authorization header")
+    print(request.authorization)
+    return ""
+
+if __name__ == "__main__":
+    app.run()
---
+++ Chapter02/flask_yaml.py
@@ -0,0 +1,19 @@
+# Chapter02
+# flask_print.py
+from flask import Flask
+import yaml # PyYAML 필요함.
+
+app = Flask(__name__)
+
+def yamlify(data, status=200, headers=None):
+    _headers = {'Content-Type': 'application/x-yaml'}
+    if headers is not None:
+        _headers.update(headers)
+    return yaml.safe_dump(data), status, _headers
+
+@app.route('/api')
+def my_microservice():
+    return yamlify(['Hello', 'YAML', 'World!'])
+
+if __name__ == '__main__':
+    app.run()
