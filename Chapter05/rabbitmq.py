fd1bb2ff3da3a588cebab30534f1a73cd77e8d97
surinkim <nnhope@hotmail.com>
2018-01-31 11:30:37 +0900

    Add rabbitmq.py

---
+++ Chapter05/rabbitmq.py
@@ -0,0 +1,18 @@
+# Chapter05
+# rabbitmq.py
+from pika import BlockingConnection, BasicProperties
+
+def message(topic, message):
+    connection = BlockingConnection()
+    try:
+        channel = connection.channel()
+        props = BasicProperties(content_type='text/plain', delivery_mode=1)
+        channel.basic_publish('incoming', topic, message, props)
+    finally:
+        connection.close()
+
+# race.34 메시지 전송
+message('race.34', 'We have some results!')
+
+# training.12 메시지 전송
+message('training.12', "It's time to do your long run")
