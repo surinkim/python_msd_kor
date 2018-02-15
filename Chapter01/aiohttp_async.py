1c775a92040b2e72360a194e39a6769eaacc0a9d
Hyunuk <nnhope@hotmail.com>
2018-02-15 15:11:45 +0900

    Update Chapter01

---
+++ Chapter01/aiohttp_async.py
@@ -0,0 +1,12 @@
+# Chapter01
+# aiohttp_async.py
+from aiohttp import web
+import time
+
+async def handle(request):
+    return web.json_response({'time': time.time()})
+
+if __name__ == '__main__':
+    app = web.Application()
+    app.router.add_get('/', handle)
+    web.run_app(app)
