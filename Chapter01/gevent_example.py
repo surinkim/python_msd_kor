ecf437ca855d9b35e3c7a068df13d3499a132909
Hyunuk <nnhope@hotmail.com>
2018-02-17 20:35:41 +0900

    Update Chapter01

---
+++ Chapter01/gevent_example.py
@@ -0,0 +1,7 @@
+from gevent import monkey; monkey.patch_all()
+
+def application(environ, start_response):
+    headers = [('Content-type', 'application/json')]
+    start_response('200 OK', headers)
+    # 소켓으로 필요한 작업을 한다.
+    return result
