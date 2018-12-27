11784e31c176adb6b49d783e5e4317762eafad22
Hyunuk <nnhope@hotmail.com>
2018-12-28 00:22:21 +0900

    Update Chapter03/test_request_01.py

--- Chapter03/test_request_01.py
+++ Chapter03/test_request_01.py
@@ -3,27 +3,28 @@
 import requests
 import bugzilla
 
+
 class MyBugzilla:
-    def __init__(self, account, server ='https://bugzilla.mozilla.org'):
+    def __init__(self, account, server='https://bugzilla.mozilla.org'):
         self.account = account
         self.server = server
         self.session = requests.Session()
-        
+
     def bug_link(self, bug_id):
         return '%s/show_bug.cgi?id=%s' % (self.server, bug_id)
 
     def get_new_bugs(self):
         call = self.server + '/rest/bug'
-        params = {'assigned_to': self.account, 'status': 'NEW','limit': 10}
-        
+        params = {'assigned_to': self.account, 'status': 'NEW', 'limit': 10}
+
         try:
-        	res = self.session.get(call, params=params).json()
+            res = self.session.get(call, params=params).json()
         except requests.exceptions.ConnectionError:
             res = {'bugs': []}
 
         def _add_link(bug):
             bug['link'] = self.bug_link(bug['id'])
             return bug
-        
+
         for bug in res['bugs']:
             yield _add_link(bug)
