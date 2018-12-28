36709385db55279428f31d04108272424d164786
surinkim <nnhope@hotmail.com>
2018-12-28 18:34:10 +0900

    Update Chapter05/synchronouscalls.py

--- Chapter05/synchronouscalls.py
+++ Chapter05/synchronouscalls.py
@@ -7,5 +7,5 @@ s.headers['Content-Type'] = 'application/json'
 s.auth = 'tarek', 'password'
 
 # 호출이 일어날 때, 헤더와 인증 정보가 모두 설정된다.
-s.get('http://localhost:5000/api').json())
+s.get('http://localhost:5000/api').json()
 s.get('http://localhost:5000/api2').json()
