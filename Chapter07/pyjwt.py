12d3d24cc5acf52065ef764519a4f6d1ab2f3e9f
Hyunuk <nnhope@hotmail.com>
2019-01-06 23:27:51 +0900

    Update Chapter07

--- Chapter07/pyjwt.py
+++ Chapter07/pyjwt.py
@@ -5,8 +5,8 @@ import jwt
 def create_token(alg='HS256', secret='secret', **data):
     return jwt.encode(data, secret, algorithm=alg)
 
-def read_token(token, secret='secret'):
-    return jwt.decode(token, secret)
+def read_token(token, secret='secret', algs=['HS256']):
+    return jwt.decode(token, secret, algorithms=algs)
 
 token = create_token(some='data', inthe='token')
 print(token)
